bb6033d36d6c287962abd111314538a0e834ee74
Pukiediederik <diederik.boeren@gmail.com>
2022-06-03 11:44:52 +0100

    normified actions.c

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: drobert- <drobert-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 13:20:54 by drobert-          #+#    #+#             */
-/*   Updated: 2022/06/02 14:19:32 by drobert-         ###   ########.fr       */
+/*   Updated: 2022/06/03 11:44:26 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void action_think(t_vars *vars, t_ulong time)
+void	action_think(t_vars *vars, t_ulong time)
 {
 	print_status(vars, "is thinking");
 	sleep_until(get_time() + time, vars);
 	pthread_mutex_lock(&vars->data->m_fullamount);
-	if (vars->data->has_died || vars->data->full_amount == vars->data->num_of_philos) {
+	if (vars->data->has_died
+		|| vars->data->full_amount == vars->data->num_of_philos)
+	{
 		pthread_mutex_unlock(&vars->data->m_fullamount);
-		return;
+		return ;
 	}
 	pthread_mutex_unlock(&vars->data->m_fullamount);
 	action_eat(vars);
 }
 
-void action_eat(t_vars *vars)
+void	action_eat(t_vars *vars)
 {
 	pthread_mutex_lock(vars->philo->l_fork);
 	print_status(vars, "has taken their left fork");
@@ -39,36 +41,40 @@ void action_eat(t_vars *vars)
 	sleep_until(get_time() + vars->data->time_to_eat, vars);
 	pthread_mutex_unlock(vars->philo->l_fork);
 	pthread_mutex_unlock(vars->philo->r_fork);
-	if (vars->philo->times_eaten == vars->data->max_eat) {
+	if (vars->philo->times_eaten == vars->data->max_eat)
+	{
 		pthread_mutex_lock(&vars->data->m_fullamount);
 		vars->data->full_amount++;
 		pthread_mutex_unlock(&vars->data->m_fullamount);
 	}
 	pthread_mutex_lock(&vars->data->m_fullamount);
-	if (vars->data->full_amount == vars->data->num_of_philos || vars->data->has_died) {
+	if (vars->data->has_died
+		|| vars->data->full_amount == vars->data->num_of_philos)
+	{
 		pthread_mutex_unlock(&vars->data->m_fullamount);
-		return;
+		return ;
 	}
 	pthread_mutex_unlock(&vars->data->m_fullamount);
 	action_sleep(vars);
 }
 
-void action_sleep(t_vars *vars)
+void	action_sleep(t_vars *vars)
 {
 	print_status(vars, "is sleeping");
 	sleep_until(get_time() + vars->data->time_to_sleep, vars);
 	pthread_mutex_lock(&vars->data->m_fullamount);
-	if (vars->data->has_died || vars->data->full_amount == vars->data->num_of_philos) {
+	if (vars->data->has_died
+		|| vars->data->full_amount == vars->data->num_of_philos)
+	{
 		pthread_mutex_unlock(&vars->data->m_fullamount);
-		return;
+		return ;
 	}
 	pthread_mutex_unlock(&vars->data->m_fullamount);
-	action_think(vars, vars->data->time_to_eat *
-		(1 + (vars->data->num_of_philos % 2))
-		- vars->data->time_to_sleep);
+	action_think(vars, vars->data->time_to_eat
+		* (1 + (vars->data->num_of_philos % 2)) - vars->data->time_to_sleep);
 }
 
-void action_die(t_vars *vars)
+void	action_die(t_vars *vars)
 {
 	pthread_mutex_lock(&vars->data->m_death);
 	if (!vars->data->has_died)
@@ -77,5 +83,5 @@ void action_die(t_vars *vars)
 			get_time() - vars->philo->start_time, vars->philo->id);
 		vars->data->has_died = 1;
 	}
-	pthread_mutex_unlock( &vars->data->m_death);
+	pthread_mutex_unlock(&vars->data->m_death);
 }
