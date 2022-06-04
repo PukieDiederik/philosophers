3d2722a5e995a30b5037e755a008b5c4e4c09787
Pukiediederik <diederik.boeren@gmail.com>
2022-06-04 16:40:07 +0100

    moved philos to seperate array

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: drobert- <drobert-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 14:09:00 by drobert-          #+#    #+#             */
-/*   Updated: 2022/06/03 14:46:18 by drobert-         ###   ########.fr       */
+/*   Updated: 2022/06/04 16:39:08 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,8 @@ void	*philosopher(void *v)
 	return (0);
 }
 
-t_philo	*create_philo(int id, t_data *data, pthread_mutex_t *forks)
+t_philo	*create_philo(int id, t_data *data, pthread_mutex_t *forks, t_philo *philo)
 {
-	t_philo	*philo;
-
-	philo = malloc(sizeof(t_philo));
-	if (!philo)
-		return (0);
 	philo->id = id + 1;
 	philo->time_last_eat = get_time();
 	philo->start_time = get_time();
@@ -54,7 +49,7 @@ int destroy_forks(pthread_mutex_t *forks, int i)
 	return (1);
 }
 
-int	init_fork_philos(pthread_t *philos, pthread_mutex_t *forks, t_data *data, t_vars *vars)
+int	init_fork_philos(pthread_t *philosophers, pthread_mutex_t *forks, t_data *data, t_vars *vars, t_philo *philos)
 {
 	int		i;
 
@@ -65,9 +60,9 @@ int	init_fork_philos(pthread_t *philos, pthread_mutex_t *forks, t_data *data, t_
 	i = -1;
 	while (++i < data->num_of_philos)
 	{
-		(vars + i)->philo = create_philo(i, data, forks);
+		(vars + i)->philo = create_philo(i, data, forks, philos + i);
 		(vars + i)->data = data;
-		pthread_create(philos + i, 0, &philosopher, vars + i);
+		pthread_create(philosophers + i, 0, &philosopher, vars + i);
 	}
 	return (0);
 }
@@ -76,6 +71,7 @@ int	main(int argc, char **argv)
 {
 	t_vars			*vars;
 	t_data			*data;
+	t_philo			*philos;
 	pthread_mutex_t	*forks;
 	pthread_t		*philosophers;
 	int				i;
@@ -86,9 +82,10 @@ int	main(int argc, char **argv)
 	if (!data)
 		return (1);
 	forks = malloc(sizeof(pthread_mutex_t) * data->num_of_philos);
+	philos = malloc(sizeof(t_philo) * data->num_of_philos);
 	philosophers = malloc(sizeof(pthread_t) * data->num_of_philos);
 	vars = malloc(sizeof(t_vars) * data->num_of_philos);
-	if (!forks || !philosophers || !vars || init_fork_philos(philosophers, forks, data, vars))
+	if (!forks || !philosophers || !vars || !philos || init_fork_philos(philosophers, forks, data, vars, philos))
 	{
 		if (vars)
 		{
@@ -101,6 +98,7 @@ int	main(int argc, char **argv)
 		free(data);
 		free(forks);
 		free(philosophers);
+		free(philos);
 		return (1);
 	}
 	i = -1;
@@ -109,10 +107,8 @@ int	main(int argc, char **argv)
 	i = -1;
 	while (++i < data->num_of_philos)
 		pthread_mutex_destroy(forks + i);
-	i = -1;
-	while (++i < data->num_of_philos)
-		free((vars + i)->philo);
 	data_destroy(data);
+	free(philos);
 	free(philosophers);
 	free(forks);
 	free(vars);
